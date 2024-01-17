50c5c879dbc3af1692052798e1b9e841fe186a04
Syed Maruf Ali <syedmarufali23@gmail.com>
2024-01-17 16:57:03 +0530

    Updated

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,11 +1,16 @@
+// Author: Sdmrf
 import { Text, View } from 'react-native'
 import React, { Component } from 'react'
+
+// Screen Imports
+import Homescreen from './screens/Homescreen'
 
 export class App extends Component {
   render() {
     return (
       <View>
         <Text>App</Text>
+        <Homescreen />
       </View>
     )
   }
