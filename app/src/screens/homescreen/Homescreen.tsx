f6d4cc28b8a453de2bbcd3c0a8d939d10859ed37
Syed Maruf Ali <syedmarufali23@gmail.com>
2024-01-18 18:07:10 +0530

    Homesreen work in progress

--- app/src/screens/homescreen/Homescreen.tsx
+++ app/src/screens/homescreen/Homescreen.tsx
@@ -1,18 +1,69 @@
 // Imports
-import { View, Text, Image } from 'react-native'
-import React from 'react'
+import React from 'react';
+import { View, Text, StyleSheet, Dimensions } from 'react-native';
 
-// File Imports
-import { icons } from '../../constants'
-
+// Constants
+import { colors } from '../../constants';
 
 const Homescreen = () => {
   return (
-    <View>
-      <Text>Home</Text>
-      <Image source={icons.instagram} />
+    <View style={styles.main}>
+      <Container />
+      <Wrapper />
     </View>
-  )
-}
+  );
+};
 
-export default Homescreen
+const Container = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Container</Text>
+    </View>
+  );
+};
+
+const Wrapper = () => {
+  return (
+    <View style={styles.wrapper}>
+      <View style={styles.circle} />
+      <Text>Wrapper</Text>
+    </View>
+  );
+};
+
+// Dimensions
+const { width, height } = Dimensions.get('screen');
+
+const styles = StyleSheet.create({
+  main: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: colors.primary,
+  },
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    backgroundColor: colors.primary,
+    height,
+    width,
+  },
+  wrapper: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: colors.secondary,
+    height: height * 0.7,
+    width,
+  },
+  circle: {
+    width: width * (2 / 3),
+    height: width * (2 / 3),
+    borderRadius: width * (1 / 3),
+    backgroundColor: colors.primary,
+    position: 'absolute',
+    top: -width * (1 / 3),
+  },
+});
+
+export default Homescreen;
